2e8643d83ad8f1012d32ed8c4585d3c757635b2f
kiransumbe <155735627+kiransumbe@users.noreply.github.com>
2024-01-08 22:43:49 +0530

    Create arithematic.java

---
+++ arithematic.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+class Accept_no_from_user
+{
+public static void main(String[] args)
+{
+int num1,int num2,res;
+Scanner sc=new Scanner(System.in);
+
+System.out.println("Enter two numbers");
+num1=sc.nextint();
+num2=sc.nextint();
+res=num1+num2;
+System.out.println("Add is="+res);
+res=num1-num2;
+System.out.println("sub is="+res);
+res=num1*num2;
+System.out.println("mul is="+res);
+res=num1/num2;
+System.out.println("div is="+res);
+res=num1%num2;
+System.out.println("mod is="+res);
+
+
+}
+}import java.util.Scanner;
+
+class Accept_no_from_user
+{
+public static void main(String[] args)
+{
+int num1,int num2,res;
+Scanner sc=new Scanner(System.in);
+
+System.out.println("Enter two numbers");
+num1=sc.nextint();
+num2=sc.nextint();
+res=num1+num2;
+System.out.println("Add is="+res);
+res=num1-num2;
+System.out.println("sub is="+res);
+res=num1*num2;
+System.out.println("mul is="+res);
+res=num1/num2;
+System.out.println("div is="+res);
+res=num1%num2;
+System.out.println("mod is="+res);
+
+
+}
+}
